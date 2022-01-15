b7620ac3ca5a415ffd590adc473de79ce0066c2e
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-01-15 10:58:25 -0700

    fixing dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
-const dotenv = require('dotenv')
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('dotenv')
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: process.env.ALCHEMY_API,
+      url: process.env.ALCHEMY_API.toString(),
       accounts: [process.env.RINKEBY_KEY],
     },
   },
